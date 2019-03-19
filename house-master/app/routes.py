6d28cc30cefb10e11d5a0dec91bd4945da168b76
RenAugs <38149959+RenAugs@users.noreply.github.com>
2019-03-19 13:37:36 +0530

    Add files via upload

---
+++ house-master/app/routes.py
@@ -0,0 +1,96 @@
+from flask import Flask,render_template,request,session,logging,url_for,redirect,flash
+from app import app, db, bcrypt
+from app.forms import RegistrationForm,LoginForm,AadharForm,UploadAadharForm,ForgotForm
+from app.models import User,Aadhar
+from flask_login import login_user,current_user,logout_user,login_required
+import app.mod_ocr.aad_ocr as ado
+import os
+from werkzeug import secure_filename
+from flask_login import login_user,current_user,logout_user
+import nexmo
+import datetime
+import random
+
+
+# APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+UPLOAD_FOLDER = os.path.basename('./uploads/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+client=nexmo.Client(key='d6ab2286', secret='2StTxLOOxYq5OaxF')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        feedback=Feedback(feed=form.feed.data)
+        db.session.add(feed)
+        db.session.commit()
+        flash(f'Feedback submitted successfully!','success')
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data,fname=form.fname.data,mname=form.mname.data,lname=form.lname.data,mobile=form.mobile.data,email=form.email.data,password=hash)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created successfully {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    else:
+        print(form.errors)
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+    
+    
+   
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+       
+
+@app.route("/forgotpassword",methods=['GET','POST'])
+def index():
+    form=ForgotForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        # number="91"+str(user.mobile)
+        number='919833760985'
+        Message=str(random.randint(100000,999999))
+        hashed_password = bcrypt.generate_password_hash(Message).decode('utf-8')
+        user.password=hashed_password
+        db.session.commit()
+        response=client.send_message({'from': 'Nexmo', 'to':number,'text':'Your temporary password is '+Message})
+
+        return redirect(url_for('login'))
+    else:
+         print(form.errors)
+    return render_template('forgot.html',title='Message',form=form)
+
